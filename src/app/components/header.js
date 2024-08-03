e9dede007d96f461cf239e1175d0226d93eab8fb
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-08-03 21:44:56 +0200

    First commit

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
     return (
-        <div style={{width: "100vw", height: 80, backgroundColor: "#fbfbfc"}}>
+        <div style={{ width: "100vw", height: 80, backgroundColor: "#fbfbfc" }}>
             <div style={{
                 margin: "0 auto",
                 maxWidth: 1280,
@@ -12,14 +13,17 @@ const Header = () => {
                 alignItems: "center"
             }}>
 
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <img src="/acquiredLogo.svg" alt="Logo" style={{height: 31, width: 140, marginRight: 20}}/>
-
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <Link href="https://www.acuired.com" legacyBehavior>
+                        <a>
+                            <img src="/acquiredLogo.svg" alt="Logo" style={{ height: 31, width: 140, marginRight: 20 }} />
+                        </a>
+                    </Link>
                 </div>
 
-                <div style={{backgroundColor:"#000", padding:"14px 20px" }}>
+                {/*<div style={{backgroundColor: "#000", padding: "14px 20px"}}>
                     <button style={ctaButtonStyle}>Book a Call</button>
-                </div>
+                </div>*/}
 
             </div>
         </div>
@@ -31,7 +35,7 @@ const buttonStyle = {
     padding: "10px 20px",
     fontSize: 16,
     lineHeight: 20,
-    weight: "500",
+    fontWeight: "500",
     backgroundColor: "#f9f9fb",
     border: "none",
     color: "#000",
