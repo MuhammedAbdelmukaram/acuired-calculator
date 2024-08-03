48af72df1d134e9672ad0724a11ea43b3daf2c78
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-08-03 22:33:04 +0200

    First commit

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,56 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './header.module.css';
 
 const Header = () => {
     return (
-        <div style={{ width: "100vw", height: 80, backgroundColor: "#fbfbfc" }}>
-            <div style={{
-                margin: "0 auto",
-                maxWidth: 1280,
-                height: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center"
-            }}>
-
-                <div style={{ display: "flex", alignItems: "center" }}>
+        <div className={styles.container}>
+            <div className={styles.innerContainer}>
+                <div className={styles.logoContainer}>
                     <Link href="https://www.acuired.com" legacyBehavior>
                         <a>
-                            <img src="/acquiredLogo.svg" alt="Logo" style={{ height: 31, width: 140, marginRight: 20 }} />
+                            <img src="/acquiredLogo.svg" alt="Logo" className={styles.logo} />
                         </a>
                     </Link>
                 </div>
 
-                {/*<div style={{backgroundColor: "#000", padding: "14px 20px"}}>
-                    <button style={ctaButtonStyle}>Book a Call</button>
+                {/*<div className={styles.ctaContainer}>
+                    <button className={styles.ctaButton}>Book a Call</button>
                 </div>*/}
-
             </div>
         </div>
     );
-};
-
-const buttonStyle = {
-    marginRight: 10,
-    padding: "10px 20px",
-    fontSize: 16,
-    lineHeight: 20,
-    fontWeight: "500",
-    backgroundColor: "#f9f9fb",
-    border: "none",
-    color: "#000",
-    borderRadius: 5,
-    cursor: "pointer"
-};
-
-const ctaButtonStyle = {
-    backgroundColor: "#000000",
-    fontWeight: "bold",
-    fontSize: 16,
-    color: "#fff",
-    border: "none",
-    borderRadius: 5,
-    cursor: "pointer"
 };
 
 export default Header;
--- src/app/components/results.js
+++ src/app/components/results.js
@@ -3,7 +3,7 @@ import styles from './results.module.css'
 
 const Results = ({ results }) => {
     return (
-        <div style={{borderTop:"3px solid #000", width:"100vw", display:"flex", justifyContent:"space-around", alignItems:"center", height:"100%", paddingTop:50}}>
+        <div className={styles.resultWrapper} >
             <div className={styles.results}>
                 <span className={styles.result}>{results.totalRevenue ? `$${results.totalRevenue}` : 'N/A'}</span>
                 <span className={styles.label}>Total Revenue</span>
